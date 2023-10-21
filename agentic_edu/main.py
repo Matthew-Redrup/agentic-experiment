37132d8ecdbbda3558d9592ebba1ddd5adcd01f8
Matthew Redrup <matthew.redrup@gmail.com>
2023-10-22 00:00:42 +1100

    aider: Updated `main()` function to parse the prompt parameter using `argparse`, get table definitions for the prompt, create calls to `llm.add_cap_ref()` to update the prompt, call `llm.prompt()` to get a prompt response, parse the SQL response, and call `db.run_sql()` with the parsed SQL.
    # Aider chat conversation:
    
    ASSISTANT: To update the `main()` function in `agentic_edu/main.py` based on the comments, we need to:
    
    1. Parse the prompt parameter using `argparse`.
    2. Get the table definitions for the prompt using `db.get_table_definitions_for_prompt()`.
    3. Create two calls to `llm.add_cap_ref()` to update the current prompt passed in from the command line.
    4. Call `llm.prompt()` to get a `prompt_response` variable.
    5. Parse the SQL response from `prompt_response` using the SQL query delimiter '_________'.
    6. Call `db.run_sql()` with the parsed SQL.
    
    Here is the updated `main()` function:
    
    ```python
    agentic_edu/main.py
    <<<<<<< HEAD
    def main():
        # parse prompt param using arg parse
    
        with PostgresManager() as db:
            db.connect_with_url(DB_URL) # 'postgresql://user:secret@localhost:5432/mydatabase'
            users_table = db.get_all("users")
    
            print("users_table", users_table)
    
            # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
    
            # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
    
            # call llm.prompt to get a prompt_response variable
    
            # parse sql response from prompt_response using SQL_QUERY_DELIMITER '_________'
    
            # call db_manager.run_sql() with the parsed sql
    
        pass
    =======
    import argparse
    
    def main():
        parser = argparse.ArgumentParser()
        parser.add_argument("prompt", help="The prompt for the OpenAI API")
        args = parser.parse_args()
    
        with PostgresManager() as db:
            db.connect_with_url(DB_URL) # 'postgresql://user:secret@localhost:5432/mydatabase'
            table_definitions = db.get_table_definitions_for_prompt()
    
            prompt_with_table_definitions = llm.add_cap_ref(args.prompt, "Here are the table definitions:", "TABLE_DEFINITIONS", table_definitions)
            prompt_response = llm.prompt(prompt_with_table_definitions)
    
            sql_query = prompt_response.split('_________')[1]
            db.run_sql(sql_query)
    >>>>>>> updated
    ```
    
    This code assumes that the SQL query is the second part of the `prompt_response` when split by '_________'. If the SQL query is in a different part of the `prompt_response`, you will need to adjust the index used in the `split()` call.

--- agentic_edu/main.py
+++ agentic_edu/main.py
@@ -10,26 +10,22 @@ assert os.environ.get("OPENAI_API_KEY"), "OPENAI_API_KEY not found in .env file"
 DB_URL = os.environ.get("DATABASE_URL")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
+import argparse
+
 def main():
-    # parse prompt param using arg parse
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="The prompt for the OpenAI API")
+    args = parser.parse_args()
+
     with PostgresManager() as db:
         db.connect_with_url(DB_URL) # 'postgresql://user:secret@localhost:5432/mydatabase'
-        users_table = db.get_all("users")
-        
-        print("users_table", users_table)
-    
-        # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
-    
-        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
-    
-        # call llm.prompt to get a prompt_response variable
-    
-        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '_________'
-    
-        # call db_manager.run_sql() with the parsed sql
-    
-    pass
+        table_definitions = db.get_table_definitions_for_prompt()
+
+        prompt_with_table_definitions = llm.add_cap_ref(args.prompt, "Here are the table definitions:", "TABLE_DEFINITIONS", table_definitions)
+        prompt_response = llm.prompt(prompt_with_table_definitions)
+
+        sql_query = prompt_response.split('_________')[1]
+        db.run_sql(sql_query)
 
 if __name__ == '__main__':
     main()
