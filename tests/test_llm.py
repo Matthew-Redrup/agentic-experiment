2d4a906ce66e81e53343c58bf5fcfdabbb376014
REDRUP-LAPTOP01 <matthew.redrup@gmail.com>
2023-11-07 23:42:40 +1100

    Update test llm

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -1,33 +1,51 @@
+import unittest
+from unittest.mock import patch, MagicMock, call
 import os
-import pytest
-from unittest.mock import patch, MagicMock
-from agentic_edu.modules.llm import safe_get, response_parser, prompt, add_cap_ref
+from agentic_edu.modules import llm  # assuming the llm.py is in the same directory
 
 
-def test_safe_get():
-    data = {"a": {"b": [{"c": 1}]}}
-    assert safe_get(data, "a.b.0.c") == 1
-    assert safe_get(data, "a.b") == [{"c": 1}]
-    assert safe_get(data, "a") == {"b": [{"c": 1}]}
-    with pytest.raises(KeyError):
-        safe_get(data, "a.b.1.c")
+class TestLLMMethods(unittest.TestCase):
+    @patch.dict(os.environ, {"OPENAI_API_KEY": "test_key"})
+    def test_environment_variable(self):
+        self.assertEqual(os.environ.get("OPENAI_API_KEY"), "test_key")
+
+    def test_safe_get(self):
+        data = {"a": {"b": [{"c": 1}]}}
+        self.assertEqual(llm.safe_get(data, "a.b.0.c"), 1)
+        self.assertEqual(llm.safe_get(data, "a.b.0.d"), None)
+
+    from unittest.mock import call  # Add this line at the top of your file
+
+    @patch("agentic_edu.modules.llm.response_parser", return_value="test_content")
+    @patch("agentic_edu.modules.llm.openai.ChatCompletion.create")
+    def test_prompt(self, mock_create, mock_response_parser):
+        mock_create.return_value = MagicMock(
+            choices=[MagicMock(message=MagicMock(content="test_content"))]
+        )
+        response = llm.prompt("test_prompt")
+        self.assertEqual(response, "test_content")
+
+    def test_add_cap_ref(self):
+        prompt = "Refactor this code."
+        prompt_suffix = "Make it more readable using this EXAMPLE."
+        cap_ref = "EXAMPLE"
+        cap_ref_content = "def foo():\n    return True"
+        expected = "Refactor this code. Make it more readable using this EXAMPLE.\n\nEXAMPLE\n\ndef foo():\n    return True"
+        self.assertEqual(
+            llm.add_cap_ref(prompt, prompt_suffix, cap_ref, cap_ref_content), expected
+        )
+
+    @patch("agentic_edu.modules.llm.tiktoken.get_encoding")
+    def test_count_tokens(self, mock_get_encoding):
+        mock_get_encoding.return_value = MagicMock(
+            encode=MagicMock(return_value=[1, 2, 3])
+        )
+        self.assertEqual(llm.count_tokens("test_text"), 3)
+
+    @patch("agentic_edu.modules.llm.count_tokens", return_value=2000)
+    def test_estimate_price_and_tokens(self, mock_count_tokens):
+        self.assertEqual(llm.estimate_price_and_tokens("test_text"), (0.12, 2000))
 
 
-def test_response_parser():
-    response = {"choices": [{"message": {"content": "test content"}}]}
-    assert response_parser(response) == "test content"
-
-
-@patch("openai.ChatCompletion.create")
-def test_prompt(mock_create):
-    mock_create.return_value = {"choices": [{"message": {"content": "test content"}}]}
-    assert prompt("test prompt") == "test content"
-
-
-def test_add_cap_ref():
-    prompt_str = "Refactor this code."
-    prompt_suffix = "Make it more readable using this EXAMPLE."
-    cap_ref = "EXAMPLE"
-    cap_ref_content = "def foo():\n    return True"
-    expected = "Refactor this code. Make it more readable using this EXAMPLE.\n\nEXAMPLE\n\ndef foo():\n    return True"
-    assert add_cap_ref(prompt_str, prompt_suffix, cap_ref, cap_ref_content) == expected
+if __name__ == "__main__":
+    unittest.main()
