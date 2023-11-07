733cba73d8c43e1b48884dea703cf4d501f33b30
REDRUP-LAPTOP01 <matthew.redrup@gmail.com>
2023-11-08 00:02:50 +1100

    Updated test orchestrator

--- tests/test_orchestrator.py
+++ tests/test_orchestrator.py
@@ -1,85 +1,77 @@
-# tests/test_orchestrator.py
-
-# import pytest
+import unittest
+from unittest.mock import MagicMock, patch, mock_open
 from agentic_edu.modules.orchestrator import Orchestrator
-from autogen import ConversableAgent
-from unittest.mock import patch
 
 
-def test_add_message():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-    orchestrator.add_message("Hello, world!")
-    assert orchestrator.messages[-1] == "Hello, world!"
+class TestOrchestrator(unittest.TestCase):
+    def setUp(self):
+        self.mock_agent = MagicMock()
+        self.mock_instruments = MagicMock()
+        self.orchestrator = Orchestrator(
+            "test", [self.mock_agent, self.mock_agent], self.mock_instruments
+        )
+
+    def test_init(self):
+        self.assertEqual(self.orchestrator.name, "test")
+        self.assertEqual(self.orchestrator.agents, [self.mock_agent, self.mock_agent])
+        self.assertEqual(self.orchestrator.instruments, self.mock_instruments)
+        self.assertEqual(self.orchestrator.messages, [])
+        self.assertEqual(self.orchestrator.chats, [])
+
+    def test_send_message(self):
+        self.orchestrator.send_message(self.mock_agent, self.mock_agent, "Hello")
+        self.assertEqual(len(self.orchestrator.chats), 1)
+        self.assertEqual(self.orchestrator.chats[0].message, "Hello")
+
+    def test_add_message(self):
+        self.orchestrator.add_message("Hello")
+        self.assertEqual(len(self.orchestrator.messages), 1)
+        self.assertEqual(self.orchestrator.messages[0], "Hello")
+
+    def test_get_message_as_str(self):
+        self.orchestrator.add_message("Hello")
+        self.orchestrator.add_message("World")
+        self.assertEqual(self.orchestrator.get_message_as_str(), "HelloWorld")
+
+    def test_get_cost_and_tokens(self):
+        self.orchestrator.add_message("Hello")
+        cost, tokens = self.orchestrator.get_cost_and_tokens()
+        self.assertTrue(isinstance(cost, float))
+        self.assertTrue(isinstance(tokens, int))
+
+    def test_has_functions(self):
+        pass
+
+    def test_basic_chat(self):
+        self.orchestrator.basic_chat(self.mock_agent, self.mock_agent, "Hello")
+        self.assertEqual(len(self.orchestrator.chats), 1)
+        self.assertEqual(self.orchestrator.chats[0].message, "Hello")
+
+    def test_memory_chat(self):
+        self.orchestrator.memory_chat(self.mock_agent, self.mock_agent, "Hello")
+        self.assertEqual(len(self.orchestrator.chats), 2)
+
+    def test_self_function_chat(self):
+        pass
+
+    @patch("builtins.open", new_callable=mock_open)
+    def test_spy_on_agents(self, mock_file):
+        self.orchestrator.spy_on_agents()
+        self.assertTrue(mock_file.called)
+
+    def test_sequential_conversation(self):
+        pass
+
+    def test_broadcast_conversation(self):
+        self.mock_agent.name = "Mock Agent"
+        self.orchestrator.broadcast_conversation("Hello")
+        self.assertEqual(len(self.orchestrator.chats), 2)
+        self.assertEqual(self.orchestrator.chats[0].message, "Hello")
+        self.assertEqual(self.orchestrator.chats[1].message, "Hello")
+
+    def test_round_robin_conversation(self):
+        pass
 
 
-def test_has_functions():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-    assert orchestrator.has_functions(agent1) == (agent1._function_map is not None)
-
-
-def test_basic_chat():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-
-    with patch.object(ConversableAgent, "generate_reply", return_value="Hello, world!"):
-        responses = orchestrator.basic_chat(agent1, agent2, "Hello, world!")
-        assert responses[0] == "basic_chat: Test Agent 1 -> Test Agent 2"
-        assert "basic_chat(): replied with: Hello, world!" in responses[1]
-
-
-def test_memory_chat():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-
-    with patch.object(ConversableAgent, "generate_reply", return_value="Hello, world!"):
-        responses = orchestrator.memory_chat(agent1, agent2, "Hello, world!")
-        assert responses[0] == "memory_chat: Test Agent 1 -> Test Agent 2"
-
-
-def test_function_chat():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-
-    with patch.object(ConversableAgent, "generate_reply", return_value="Hello, world!"):
-        responses = orchestrator.function_chat(agent1, agent2, "Hello, world!")
-        assert responses[0] == "function_chat(): Test Agent 1 -> Test Agent 2"
-
-
-def test_properties():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-    orchestrator.add_message("Hello, world!")
-    assert orchestrator.total_agents == 2
-    assert orchestrator.last_message_is_dict == False
-    assert orchestrator.last_message_is_string == True
-    assert orchestrator.last_message_is_func_call == False
-    assert orchestrator.last_message_is_content == False
-    assert orchestrator.latest_message == "Hello, world!"
-
-
-def test_sequential_conversation():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-    with patch.object(ConversableAgent, "generate_reply", return_value="APPROVED"):
-        was_successful, messages = orchestrator.sequential_conversation("Hello, world!")
-        assert was_successful == True
-        assert "Hello, world!" in messages
-
-
-def test_broadcast_conversation():
-    agent1 = ConversableAgent("Test Agent 1")
-    agent2 = ConversableAgent("Test Agent 2")
-    orchestrator = Orchestrator("Test Orchestrator", [agent1, agent2])
-    with patch.object(ConversableAgent, "generate_reply", return_value="APPROVED"):
-        was_successful, messages = orchestrator.broadcast_conversation("Hello, world!")
-        assert was_successful == True
-        assert "Hello, world!" in messages
+if __name__ == "__main__":
+    unittest.main()
